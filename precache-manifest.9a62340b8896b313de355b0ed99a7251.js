69e2992b6d6361a05e8be9bc3ff4ff4cecfbe1f2
michalklos <galeo09@gmail.com>
2020-04-23 11:17:18 +0200

    Updates

---
+++ precache-manifest.9a62340b8896b313de355b0ed99a7251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c8ca0eb1bbc7ab0b634",
+    "url": "/autka-front/static/css/main.8123b115.chunk.css"
+  },
+  {
+    "revision": "1c8ca0eb1bbc7ab0b634",
+    "url": "/autka-front/static/js/main.1c8ca0eb.chunk.js"
+  },
+  {
+    "revision": "75b1188bea2af0076083",
+    "url": "/autka-front/static/css/1.97bccfcd.chunk.css"
+  },
+  {
+    "revision": "75b1188bea2af0076083",
+    "url": "/autka-front/static/js/1.75b1188b.chunk.js"
+  },
+  {
+    "revision": "5d502057bafddd95a95b",
+    "url": "/autka-front/static/js/runtime~main.5d502057.js"
+  },
+  {
+    "revision": "74fb793e9cd0f882f91bdd22371dfdd0",
+    "url": "/autka-front/index.html"
+  }
+];
