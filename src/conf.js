43aa5b9b7752aa2033b48e0282251aed23ecfdc5
michalklos <galeo09@gmail.com>
2020-04-23 11:14:02 +0200

    Added new deploy config

--- src/conf.js
+++ src/conf.js
@@ -1,9 +1,9 @@
 const test = {
-  API_URL: 'http://localhost:3001',
+  API_URL: 'http://35.225.156.136:3001',
 };
 
 const prod = {
-  API_URL: 'http://ec2-34-248-234-92.eu-west-1.compute.amazonaws.com:3001',
+  API_URL: 'http://35.225.156.136:3001',
 };
 
 const config = process.env.NODE_ENV === 'production' ? prod : test;
