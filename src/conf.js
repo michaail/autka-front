0da604018bd11e2ced4b3df8d7678ce93c4d26f2
michalklos <galeo09@gmail.com>
2020-05-05 12:14:05 +0200

    changed https to http

--- src/conf.js
+++ src/conf.js
@@ -1,9 +1,9 @@
 const test = {
-  API_URL: 'https://35.225.156.136:3001',
+  API_URL: 'http://35.225.156.136:3001',
 };
 
 const prod = {
-  API_URL: 'https://35.225.156.136:3001',
+  API_URL: 'http://35.225.156.136:3001',
 };
 
 const config = process.env.NODE_ENV === 'production' ? prod : test;
